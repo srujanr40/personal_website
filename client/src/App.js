da7d8752fd8f22fabf2af9682d110c2fbbd12b79
Srujan <srujanr40@gmail.com>
2024-03-02 11:50:25 -0800

    initial_setup

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import {
+  BrowserRouter as Router, Route, Routes,
+} from 'react-router-dom';
+import Home from './components/Home/Home'
+import About from './components/About/About'
+
+function App() {
+  return (
+      <Router>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+      </Router>
+  );
+}
+
+export default App;
