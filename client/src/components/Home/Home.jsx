9dfd99a09a3660764ac7b9ba1efc8263cdc705dc
Srujan <srujanr40@gmail.com>
2024-03-03 10:05:06 -0800

    some basic animations

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import './Home.css';
 import React, { useState, useEffect } from 'react';
 import { TypeAnimation } from 'react-type-animation';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import NavigationBar from '../NavigationBar/NavigationBar';
 
 export default function Home() {
@@ -31,7 +31,7 @@ export default function Home() {
     return (
         <div className='crt' onClick={() => setIntroDone(true)}>
             {!introDone && (
-                <motion.div className={`intro ${textDone ? 'start' : ''}`}>
+                <motion.div className={`intro ${textDone ? 'start' : ''}`} key = 'intro'>
                     <TypeAnimation
                         cursor={true}
                         sequence={[
@@ -56,7 +56,14 @@ export default function Home() {
                         speed={1}
                         repeat={0}
                     />
-                    <motion.div>
+                    <motion.div style={{width: '100%'}} 
+                        initial={{ opacity: 0, scale: 0.5 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{
+                        duration: 0.8,
+                        delay: 0.5,
+                        ease: [0, 0.71, 0.2, 1.01]
+                    }}>
                         <NavigationBar />
                     </motion.div>
                 </motion.div>
