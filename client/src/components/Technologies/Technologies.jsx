11bda7decc22d8bfd3e13d24854775497c7fa1e9
Srujan <srujanr40@gmail.com>
2024-03-06 11:16:39 -0800

    Projects (#8)
    * better animation timing
    
    * projects set up, needs hyperlinks and images
    
    * added links

--- client/src/components/Technologies/Technologies.jsx
+++ client/src/components/Technologies/Technologies.jsx
@@ -3,7 +3,6 @@ import { motion } from "framer-motion";
 
 import "./Technologies.css";
 
-
 export default function Technologies() {
     const [listAnimationStart, setListAnimationStart] = useState(false);
 
@@ -18,21 +17,34 @@ export default function Technologies() {
             transition: {
                 type: "spring",
                 bounce: 0.4,
-                duration: 0.8
+                duration: 1.0
             }
         }
     }
 
     const wipeInVariants = {
         hidden: {
-            x: '2vw', // Start off-screen to the left
             opacity: 0
         },
         visible: {
-            x: 0, // End at its natural position
             opacity: 1,
             transition: {
-                type: "tween",
+                staggerChildren: 0.2, // Delay between each child animation
+            }
+        }
+    }
+
+    // Individual item animation
+    const itemVariants = {
+        hidden: {
+            y: '2vw', 
+            opacity: 0
+        },
+        visible: {
+            y: 0, 
+            opacity: 1,
+            transition: {
+                type: "spring",
                 duration: 0.2, // Adjust the duration as needed
                 ease: "easeInOut"
             }
@@ -47,7 +59,7 @@ export default function Technologies() {
         initial='offscreen'
         whileInView='onscreen'
         viewport={{ once: true }}
-        onAnimationComplete={() => setListAnimationStart(true)} // Trigger the list animation after this animation completes
+        onAnimationComplete={() => setListAnimationStart(true)}
       >
         <div className="technologies_subtitle">
             <p>technologies</p>
@@ -58,8 +70,8 @@ export default function Technologies() {
             initial='hidden' 
             animate={listAnimationStart ? "visible" : "hidden"}
         >
-            <motion.div className="languages">
-                <p style={{fontWeight: 'bold', color: '#3d3d3d', paddingBottom: '10px'}}>Languages</p>
+            <motion.div className="languages" variants={itemVariants}>
+                <p style={{fontWeight: 'bold', color: '#3d3d3d', paddingBottom: '10px', marginTop: '30px'}}>Languages</p>
                 <p>C</p>
                 <p>Java</p>
                 <p>Python</p>
@@ -68,8 +80,8 @@ export default function Technologies() {
                 <p>SQL</p>
                 <p>HTML/CSS</p>
             </motion.div>
-            <motion.div className="frameworks">
-                <p style={{fontWeight: 'bold', color: '#3d3d3d', paddingBottom: '10px'}}>Frameworks</p>
+            <motion.div className="frameworks" variants={itemVariants}>
+                <p style={{fontWeight: 'bold', color: '#3d3d3d', paddingBottom: '10px', marginTop: '30px'}}>Frameworks</p>
                 <p>React</p>
                 <p>Node.js</p>
                 <p>Flask</p>
@@ -81,8 +93,8 @@ export default function Technologies() {
                 <p>Springboot</p>
                 <p>Express</p>
             </motion.div>
-            <motion.div className="developer_tools">
-                <p style={{fontWeight: 'bold', color: '#3d3d3d', paddingBottom: '10px'}}>Developer Tools</p>
+            <motion.div className="developer_tools" variants={itemVariants}>
+                <p style={{fontWeight: 'bold', color: '#3d3d3d', paddingBottom: '10px', marginTop: '30px'}}>Developer Tools</p>
                 <p>Git</p>
                 <p>GitHub</p>
                 <p>Linux</p>
@@ -94,8 +106,8 @@ export default function Technologies() {
                 <p>OpenAPI/Swagger</p>
                 <p>Postman</p>
             </motion.div>
-            <motion.div className="libraries">
-                <p style={{fontWeight: 'bold', color: '#3d3d3d', paddingBottom: '10px'}}>Libraries</p>
+            <motion.div className="libraries" variants={itemVariants}>
+                <p style={{fontWeight: 'bold', color: '#3d3d3d', paddingBottom: '10px', marginTop: '30px'}}>Libraries</p>
                 <p>Chai</p>
                 <p>MongooseJS</p>
                 <p>Redux</p>
