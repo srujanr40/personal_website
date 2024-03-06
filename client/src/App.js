7c5375a0219769846391feeac06b5b31a47772db
Srujan <srujanr40@gmail.com>
2024-03-06 11:57:59 -0800

    works!

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import Home from './components/Home/Home'
 
 function App() {
   return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
