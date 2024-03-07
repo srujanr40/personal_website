a4fe24b1e97a11b3ca644c7577f2811fbf4554a0
Srujan <srujanr40@gmail.com>
2024-03-07 10:57:37 -0800

    updated navbar for mobile

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <div className = "button_div" style={buttonBackgroundStyle}>
-                <motion.button className='nav_button' variants={learnMoreButtonVariants} whileHover='whileHover' style={{fontWeight: 'bold'}} onClick={() => window.location.href = `${process.env.PUBLIC_URL}/srujan_resume.pdf`}>Resume</motion.button>
+                <motion.button className='nav_button' variants={learnMoreButtonVariants} whileHover='whileHover' style={{fontWeight: 'bolder'}} onClick={() => window.location.href = `${process.env.PUBLIC_URL}/srujan_resume.pdf`}>Resume</motion.button>
             </div>
             <div className = "button_div" style={buttonBackgroundStyle}>
                 <motion.button className='nav_button' variants={learnMoreButtonVariants} whileHover='whileHover' onClick={() => document.querySelector('.about').scrollIntoView({ behavior: 'smooth', block: 'center' })}>About</motion.button>
